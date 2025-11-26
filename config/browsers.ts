978c475f5853bc406b3c41d2767077e0dd397c0a
Ilya Isupov <SwinX@users.noreply.github.com>
2025-11-26 15:48:59 +0300

    fix: fixed exports format for package (#25)
    Added conditional exports as modern approach
    Restrited interface to index files only for each lib part
    Removed config examples as non-relevant content
    Ported patch for collectPageActivity action from playwright-utils

--- config/browsers.ts
+++
@@ -1,5 +0,0 @@
-import { projects as desktop } from './browsersDesktop';
-import { projects as mobile } from './browsersMobile';
-
-export const projects = [...desktop, ...mobile];
-export default projects;
