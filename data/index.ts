844b1e0eba419c0385f0244b26f879f174d052bb
Ilya Isupov <SwinX@users.noreply.github.com>
2025-12-01 17:56:06 +0300

    fix: exported data from playwright-tools (#32)
    Added interface for data module
    GlobalSettings type changed to interface

---
+++ data/index.ts
@@ -0,0 +1,12 @@
+export type { CacheSettings } from './cacheSettings';
+export { cacheSettings } from './cacheSettings';
+
+export { extraHttpHeaders } from './extraHttpHeaders';
+
+export type { GlobalSettings, PartialGlobalSettings } from './globalSettings';
+export { globalSettings } from './globalSettings';
+
+export type { StorageState } from './storageStates';
+export { setState, getState, hasState, deleteState } from './storageStates';
+
+export { testSlugs } from './testSlugs';
