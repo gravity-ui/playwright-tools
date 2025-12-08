f58925e26f41e4674c2088f6f2adbca5fdaf59a9
Ilya Isupov <swinx89@gmail.com>
2025-12-08 11:40:20 +0300

    fix: moved component-tests related code to storybook folder

--- storybook/index.ts
+++ storybook/index.ts
@@ -1,2 +1,7 @@
-export type { Cases, CasesWithName, Scenario, ScenarioName } from './types';
-export { createSmokeScenarios } from './create-smoke-scenarios';
+// Utils
+export type { Cases, CasesWithName, Scenario, ScenarioName } from './utils';
+export { createSmokeScenarios } from './utils';
+
+// Fixtures
+export type { MountFn, MountTestArgs } from './fixtures';
+export { mountFixture, TEST_WRAPPER_CLASS } from './fixtures';
