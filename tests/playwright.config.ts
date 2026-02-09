a37976e90960ce0cc579d0d6506cc65bc6248f0c
Ilya Isupov <swinx89@gmail.com>
2026-02-09 18:23:28 +0300

    fix: fixup

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -1,13 +1,28 @@
+import { resolve } from 'path';
+
 import { defineConfig, devices } from '@playwright/experimental-ct-react';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     testDir: './',
+    testMatch: '*.test.ts{,x}',
     fullyParallel: true,
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 2 : 0,
     workers: process.env.CI ? 1 : undefined,
-    reporter: [['list'], ['html']],
+    reporter: [
+        ['list'],
+        [
+            'html',
+            {
+                open: process.env.CI ? 'never' : 'on-failure',
+                outputFolder: resolve(
+                    __dirname,
+                    process.env.IS_DOCKER ? 'report-docker' : 'report',
+                ),
+            },
+        ],
+    ],
     use: {
         trace: 'on-first-retry',
         ctPort: 3100,
